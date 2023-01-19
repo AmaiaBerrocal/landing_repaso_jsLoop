aa346d0ae31e9388110641f559e056ecfba5b1ee
trontzal <trontzal@gmail.com>
2023-01-19 14:26:57 +0100

    a medias no finciona

--- custome.js
+++ custome.js
@@ -1,12 +1,24 @@
 
+document.getElementById("boton").addEventListener("click",printRangeOfNumbers)
 
-
-
+//let firstImput = document.getElementById("firstNumber").value;
+let firstNumber = 10//parseInt(firstImput)
+let secondNumber =20
 
 function printRangeOfNumbers(numFrom, numTo) {
+    let parrafo= document.createElement("p")
+    let result = ""
     for (let i = numFrom; i <= numTo; i++) {
-        alert(i);
+        console.log(i)
+        result += i + ", "
     }
+    parrafo.innerHTML = result
+    let contenedor = document.getElementById("resultado")
+    contenedor.appendChild(parrafo)
 }
 
-printRangeOfNumbers(1, 5);
+if(firstNumber < secondNumber){
+    printRangeOfNumbers(firstNumber, secondNumber)
+}else{
+    printRangeOfNumbers(secondNumber, firstNumber)
+}
