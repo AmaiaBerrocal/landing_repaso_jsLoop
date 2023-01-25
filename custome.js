730da223eab51b011f8dec6555057f7c88ff2ac3
Amaia <amaia.berrocal@gmail.com>
2023-01-25 13:01:35 +0100

    loop terminado y checkbox con botón habilitado

--- custome.js
+++ custome.js
@@ -16,7 +16,7 @@ function printRangeOfNumbers() {
 
 function calculateRangeOfNumbers(numFrom, numTo) {
     let result = "";
-    for (let i = numFrom; i <= numTo; i+=3) {
+    for (let i = numFrom; i <= numTo; i++) {
             if (i !=  numTo) {
                 result += i + ", ";
             } else {
@@ -28,19 +28,8 @@ function calculateRangeOfNumbers(numFrom, numTo) {
 
 let check = document.getElementById("cbox");
 let button = document.getElementById("boton");
-check.addEventListener("click", disbleSending)
+check.addEventListener("change", disbleSending)
 
 function disbleSending(){
-    console.log(button.disabled);
-<<<<<<< HEAD
-
-}
-=======
-    console.log(check.checked);
     button.disabled = !check.checked;
-    console.log(button.disabled);
-    console.log(check.checked);
-    
 }
-
->>>>>>> c2c4ae3b0271634e4250fdc3de46a12ee77efe60
